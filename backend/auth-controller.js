63d9b356c653269fb5e1b917e9101071a982a472
bautistaAD <aqbautista@up.edu.ph>
2023-05-25 12:29:14 +0800

    feat: added sign up function

---
+++ backend/auth-controller.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import UserSchema from "./models/user.js";
+
+const User = mongoose.model("user", UserSchema);
+
+const signUp = async (req, res) => {
+    //add adviser and application
+    const {  firstName, middleName, lastName, upMail, password, studentNumber, userType, adviser, application } = req.body;
+    bcrypt.hash(password, 10).then((hash) => {
+        User.create({
+            firstName: firstName,
+            middleName: middleName,
+            lastName: lastName,
+            upMail: upMail,
+            password: hash,
+            studentNumber: studentNumber,
+            userType: userType,
+            adviser: adviser,
+            application: application
+        })
+            .then(() => {
+                res.send({success : true});
+            })
+            .catch((err) => {
+                if (err){
+                    res.send({success : false})
+                    console.log(err);
+                }
+            })
+    })
+}
+
+
+export {signUp} ;
