e8d3973c2cd4282c24df8d7a2c1e4fb92761202e
bautistaAD <aqbautista@up.edu.ph>
2023-05-25 18:52:27 +0800

    feat: created log in function

--- backend/auth-controller.js
+++ backend/auth-controller.js
@@ -31,4 +31,39 @@ const signUp = async (req, res) => {
     })
 }
 
-export {signUp};
+const login = async (req,res) => {
+    //get email and password from the body
+    const name = req.body.firstName;
+    const email = req.body.upMail.trim();
+    const password = req.body.password;
+
+    // Check if email exists
+    const user = await User.findOne({upMail: email});
+
+     //  Scenario 1: FAIL - User doesn't exist
+    if(!user){
+        return res.send({ success: false })  
+    }
+
+    //check if password is correct
+    const dbPassword = user.password;
+    //compare using bcrypt, get pass from database and from input
+    bcrypt.compare(password, dbPassword).then((match) => {
+         // Scenario 2: FAIL - Wrong password
+        if(!match) 
+        {
+            return res.send({ success: false })  
+        }
+        else
+        {
+            // Scenario 3: SUCCESS
+            return res.send({ success: true })
+        }
+    })
+
+}
+
+const checkifloggedin = async (res,req) => {
+
+}
+export {signUp, login, checkifloggedin};
