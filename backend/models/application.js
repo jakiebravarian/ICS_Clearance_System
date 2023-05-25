132e3b6c5085999308459dc4d2915a24d0e11341
bautistaAD <aqbautista@up.edu.ph>
2023-05-25 12:28:26 +0800

    chore: change file name, comment out for testting

---
+++ backend/models/application.js
@@ -0,0 +1,26 @@
+// import mongoose from "mongoose";
+// import { Schema } from "mongoose";
+// import UserSchema from "./user.js";
+
+// const user = mongoose.model('user', UserSchema);
+// const AppSchema = new mongoose.Schema({
+//   status: { type: String, required: true },
+//   step: { type: Number, required: true },
+//   remarks: [
+//     {
+//       remark: { type: String, required: true },
+//       dateRemark: { type: String, required: true },
+//       commenter: { type: Schema.Types.ObjectId, ref: 'user', required: true },
+//       stepGivenRemark: { type: Number, required: true },
+//     },
+//   ],
+//   studentSubmission: {
+//     remarkSubmission: { type: String, required: true },
+//     dateSubmission: { type: Date, required: true },
+//     stepGivenSubmission: { type: Number, required: true },
+//   },
+// });
+
+
+
+// export default AppSchema;
