43737567f216a39e5f83045a6745e2532f23c147
bautistaAD <aqbautista@up.edu.ph>
2023-05-26 23:39:51 +0800

    chore: updated routes

--- backend/routes.js
+++ backend/routes.js
@@ -1,10 +1,10 @@
-import {signUp, login, checkIfLoggedIn} from "./auth-controller.js"
+import {signUp, login, checkifloggedin} from "./auth-controller.js"
 
 const setUpRoutes = (app) => {
     app.get("/", (req, res) => { res.send("API Home") });
     app.post("/signup", signUp);
     app.post("/login", login);
-    app.post("/checkifloggedin", checkIfLoggedIn);
+    app.post("/checkifloggedin", checkifloggedin);
   }
   
   export default setUpRoutes;
