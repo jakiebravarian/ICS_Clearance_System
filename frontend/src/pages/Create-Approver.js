a16774bab23d69ca672c150ab79594752d7bef3b
jakiebravarian <jcbrabante@up.edu.ph>
2023-05-29 02:36:41 +0800

    ui: Added proper UI response for input validation

---
+++ frontend/src/pages/Create-Approver.js
@@ -0,0 +1,148 @@
+import React from "react";
+import logo from '../assets/ICS.png';
+import '../assets/styles/Home.css';
+import { useState } from 'react';
+import { useForm } from "react-hook-form";
+
+export default function CreateApproverAccount() {
+    // added use states
+    const [firstName, setFirstName] = useState("");
+    const [middleName, setMiddleName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [upMail, setUpMail] = useState("");
+    const [password, setPassword] = useState("");
+
+    //fetch from backend
+    function handleSignUp() {
+
+    }
+
+    //functions that handle changes on each input
+    const handleFname = (e) => {
+        setFirstName(e.target.value);
+    }
+
+    const handleMname = (e) => {
+        setMiddleName(e.target.value);
+    }
+
+    const handleLname = (e) => {
+        setLastName(e.target.value);
+    }
+
+    const handleUpMail = (e) => {
+        setUpMail(e.target.value);
+    }
+
+    const handlepassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    // for form validation
+    const {
+        register,
+        reset,
+        formState: { errors },
+        handleSubmit
+    } = useForm({
+        mode: "onChange"
+    });
+
+    const onSubmit = (data) => {
+        // console.log(JSON.stringify(data));
+        try {
+            handleSignUp();
+            reset()
+        } catch (error) {
+            console.log(error)
+        }
+
+    };
+
+    return (
+        <div className="wrapper">
+            {/* Left Side */}
+            <div className="container" id="login-container">
+                <img src={logo} alt="" id="login-img" />
+                <h2 id="login-h2">Institute of Computer Science</h2>
+                <h3>CLEARANCE APPROVAL SYSTEM</h3>
+            </div>
+            {/* Right Side */}
+
+            {/* changed to form to implement reset */}
+            <form className="form" onSubmit={handleSubmit(onSubmit)}>
+                <h1 id="signup-h1">Create Approver Account</h1>
+
+                {/* First Name Input */}
+                <input
+                    type="text"
+                    id="first-name"
+                    placeholder="First Name"
+                    {...register("firstName", {
+                        onChange: handleFname,
+                        required: "Please enter Approver's First Name",
+                    })}
+                />
+                {errors.firstName && <p id="error-message">{errors.firstName.message}</p>}
+
+                {/* Middle Name Input */}
+                <input
+                    type="text"
+                    id="middle-name"
+                    placeholder="Middle Name (Optional)"
+                    {...register("middleName", {
+                        onChange: handleMname,
+                    })}
+                />
+                {errors.middleName && <p id="error-message">{errors.middleName.message}</p>}
+
+                {/* Last Name Input */}
+                <input
+                    type="text"
+                    id="last-name"
+                    placeholder="Last Name"
+                    {...register("lastName", {
+                        onChange: handleLname,
+                        required: "Please enter Approver's Last Name",
+                    })}
+                />
+                {errors.lastName && <p id="error-message">{errors.lastName.message}</p>}
+
+                {/* UP Mail Input */}
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="UP Mail"
+                    {...register("email", {
+                        onChange: handleUpMail,
+                        required: "Please enter Approver's UP Mail",
+                        pattern: {
+                            value: /^([a-z0-9]+)@up\.edu\.ph$/i,
+                            message: "Invalid UP Mail"
+                        }
+                    })}
+                />
+                {errors.email && <p id="error-message">{errors.email.message}</p>}
+
+                {/* Password Input */}
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    {...register("password", {
+                        onChange: handlepassword,
+                        required: "Please enter Approver's Password",
+                        minLength: {
+                            value: 8,
+                            message: "Password must have at least 8 characters"
+                        }
+                    })}
+                />
+                {errors.password && <p id="error-message">{errors.password.message}</p>}
+
+                <button type="submit" id="signup-button">Create</button>
+            </form>
+        </div>
+    )
+
+}
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,73 +2,58 @@ import React from "react";
 import logo from '../assets/ICS.png';
 import '../assets/styles/Home.css'
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-// class Login extends React.Component {
-
-//     render() {
-//         return (
-//             <div className="wrapper">
-//                 {/* Navigation Menu */}
-//                 <div className="navbar">
-//                     <a href="/">Home</a>
-//                     <a href="/signup">Sign Up</a>
-//                     <div class="dropdown">
-//                         <button class="dropbtn">Log in
-//                             <i class="fa fa-angle-down"></i>
-//                         </button>
-//                         <div class="dropdown-content">
-//                             <a href="/login">Student</a>
-//                             <a href="/login-approver">Approver</a>
-//                             {/* <a href="#">Admin</a> */}
-//                         </div>
-//                     </div>
-//                 </div>
-//                 {/* Left Side */}
-//                 <div className="container" id="login-container">
-//                     <img src={logo} alt="" id="login-img" />
-//                     <h2 id="login-h2">Institute of Computer Science</h2>
-//                     <h3>CLEARANCE APPROVAL SYSTEM</h3>
-//                 </div>
-//                 {/* Right Side */}
-//                 <div className="form">
-//                     <h1 id="login-h1">Welcome Back, Student!</h1>
-//                     <input type="text" placeholder="UP Mail" id="email-login" required={true} /> <br />
-//                     <input type="password" placeholder="Password" id="password-login" required={true} /> <br />
-//                     <button id="login-button">Login</button>
-//                     <p id="login-p">Don’t have an account yet? <span><a href="/signup" id="signup-link">Sign up</a></span></p>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
-export default function Login(){
+export default function Login() {
+    // added use states
     const [upMail, setUpMail] = useState("");
     const [password, setPassword] = useState("")
 
+    //fetch from backend
+    //incomplete, kulang pa yung sa cookies banda
+    function handleLogin() {
+        // fetch('http://localhost:3001/login', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({
+        //         upMail: upMail,
+        //         password: password
+        //     })
+        // })
+        //     .then(console.log);
+    }
+
+    //functions that handle changes on each input
     const handleUpMail = (e) => {
         setUpMail(e.target.value);
     }
 
     const handlePassword = (e) => {
         setPassword(e.target.value);
-    } 
-
-    //incomplete, kulang pa yung sa cookies banda
-    function handleLogin(){
-        fetch('http://localhost:3001/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            upMail: upMail,
-            password: password
-          })
-        })
-          .then(console.log);
-
     }
+
+    // for form validation
+    const {
+        register,
+        reset,
+        formState: { errors },
+        handleSubmit
+    } = useForm({
+        mode: "onChange"
+    });
+
+    const onSubmit = (data) => {
+        // console.log(JSON.stringify(data));
+        try {
+            handleLogin();
+            reset();
+        } catch (error) {
+            console.log(error)
+        }
+
+    };
 
     return (
         <div className="wrapper">
@@ -94,11 +79,43 @@ export default function Login(){
                 <h3>CLEARANCE APPROVAL SYSTEM</h3>
             </div>
             {/* Right Side */}
-            <form className="form">
+            <form className="form" onSubmit={handleSubmit(onSubmit)}>
                 <h1 id="login-h1">Welcome Back, Student!</h1>
-                <input type="text" placeholder="UP Mail" id="email-login" required={true} onChange={handleUpMail}/> <br />
-                <input type="password" placeholder="Password" id="password-login" required={true} onChange={handlePassword} /> <br />
-                <button onClick={handleLogin()} id="login-button">Login</button>
+
+                {/* UP Mail Input */}
+                <input
+                    type="email"
+                    id="email-login"
+                    placeholder="UP Mail"
+                    {...register("email", {
+                        onChange: handleUpMail,
+                        required: "Please enter your UP Mail",
+                        pattern: {
+                            value: /^([a-z0-9]+)@up\.edu\.ph$/i,
+                            message: "Invalid UP Mail"
+                        }
+                    })}
+                />
+                {errors.email && <p id="error-message">{errors.email.message}</p>}
+
+                {/* Password Input */}
+                <input
+                    type="password"
+                    id="password-login"
+                    placeholder="Password"
+                    {...register("password", {
+                        onChange: handlePassword,
+                        required: "Please enter your Password",
+                        minLength: {
+                            value: 8,
+                            message: "Password must have at least 8 characters"
+                        }
+                    })}
+                />
+                {errors.password && <p id="error-message">{errors.password.message}</p>}
+
+                <button type="submit" id="login-button">Login</button>
+
                 <p id="login-p">Don’t have an account yet? <span><a href="/signup" id="signup-link">Sign up</a></span></p>
             </form>
         </div>
