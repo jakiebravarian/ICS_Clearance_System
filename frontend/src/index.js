9f6f5a84c711d2e96203de843ead25e883bdb4fe
jrppena <jppena@up.edu.ph>
2023-05-28 22:12:50 +0800

    feat: added checkiflogged in functions and routes them to the proper paths

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import { redirect } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -13,14 +14,48 @@ import Student from './pages/Student';
 
 
 
+const checkIfLoggedInOnHome = async () => {
+  const res = await fetch("http://localhost:3001/checkifloggedin",
+    {
+      method: "POST",
+      credentials: "include" 
+    });
+
+  const payload = await res.json();
+  
+    if (payload.isLoggedIn) {
+      return redirect("/verify")
+    } else {
+      return 0
+    }
+}
+
+// Send a POST request to API to check if the user is logged in. Redirect the user back to / if not logged in
+const checkIfLoggedInOnDash = async () => {
+  const res = await fetch("http://localhost:3001/checkifloggedin",
+    {
+      method: "POST",
+      credentials: "include" 
+    });
+
+
+  const payload = await res.json();
+    if (payload.isLoggedIn) {
+      return true
+    } else {
+      return redirect("/")
+    }
+}
+
+
 const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/login', element: <Login /> },
+  { path: '/', element: <Home />,loader: checkIfLoggedInOnHome },
+  { path: '/login', element: <Login />, loader: checkIfLoggedInOnHome},
   { path: '/login-approver', element: <LoginAprrover /> },
-  { path: '/signup', element: <SignUp /> },
+  { path: '/signup', element: <SignUp />,loader: checkIfLoggedInOnHome },
   { path: '/signup-approver', element: <SignUpApprover /> },
-  { path: '/verify', element: <Verify /> },
-  { path: '/student', element: <Student /> }
+  { path: '/verify', element: <Verify />, loader: checkIfLoggedInOnDash },
+  { path: '/student', element: <Student />, loader: checkIfLoggedInOnDash }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
