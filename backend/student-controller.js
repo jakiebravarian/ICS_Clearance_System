ce22cf40fbebae36cec5a559ebe475a4128e4b96
jrppena <jppena@up.edu.ph>
2023-06-02 01:51:44 +0800

    chore: added try catch to student controller apis

--- backend/student-controller.js
+++ backend/student-controller.js
@@ -4,17 +4,28 @@ import { UserSchema } from './models/user.js';
 const Student = mongoose.model("users",UserSchema);
 
 const getAllStudents = async (req, res) => {
-    const userType = "student"; // Get the user type from the query parameter
-    const students = await Student.find({ userType: userType }); // Query the students with the specified user type
-    console.log(students);
-    res.send(students);
-};
-
-const getCurrentStudent = async (req, res) => {
-    const upMail = req.query.upMail; // Get the user type from the query parameter
-    const students = await Student.findOne({ upMail: upMail }); // Query the students with the specified user type
-    console.log(students);
-    res.send(students);
-};
+    try {
+      const userType = req.query.userType; // Get the user type from the query parameter
+      const students = await Student.find({ userType: userType }); // Query the students with the specified user type
+      console.log(students);
+      res.send(students);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  };
+  
+  const getCurrentStudent = async (req, res) => {
+    try {
+      const upMail = req.query.upMail; // Get the user email from the query parameter
+      const student = await Student.findOne({ upMail: upMail }); // Query the student with the specified email
+      console.log(student);
+      res.send(student);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  };
+  
 
 export {getAllStudents,getCurrentStudent};
