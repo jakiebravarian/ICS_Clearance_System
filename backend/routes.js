bcb3773cd91215482394786bbc1c813568fa07f7
jrppena <jppena@up.edu.ph>
2023-06-02 02:12:40 +0800

    chore: updated routes

--- backend/routes.js
+++ backend/routes.js
@@ -1,14 +1,23 @@
-import {signUp, login, checkifloggedin} from "./auth-controller.js"
-import {getAllStudents,getCurrentStudent} from "./student-controller.js"
+import { signUp, login, checkIfLoggedIn } from "./auth-controller.js";
+import { getAllStudents, getCurrentStudent, submitApplication, viewStudentClearanceStatus, updateStep, updateStudentSubmission } from "./student-controller.js";
 
 const setUpRoutes = (app) => {
-    app.get("/", (req, res) => { res.send("API Home") });
-    app.post("/signup", signUp);
-    app.post("/login", login);
-    app.post("/checkifloggedin", checkifloggedin);
+  app.get("/", (req, res) => {
+    res.send("API Home");
+  });
 
-    app.get("/get-all-students",getAllStudents);
-    app.get("/get-current-student",getCurrentStudent);
-  }
-  
-  export default setUpRoutes;
+  // Authentication routes
+  app.post("/signup", signUp);
+  app.post("/login", login);
+  app.post("/checkifloggedin", checkIfLoggedIn);
+
+  // Student routes
+  app.get("/get-all-students", getAllStudents);
+  app.get("/get-current-student", getCurrentStudent);
+  app.post("/submit-application", submitApplication);
+  app.get("/view-student-clearance-status", viewStudentClearanceStatus);
+  app.put("/update-step", updateStep);
+  app.put("/update-student-submission", updateStudentSubmission);
+};
+
+export default setUpRoutes;
