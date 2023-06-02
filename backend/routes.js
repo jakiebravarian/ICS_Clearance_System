245f12d0b163aa69265dea751dfe5afd5453cffc
bautistaAD <aqbautista@up.edu.ph>
2023-06-02 20:59:42 +0800

    feat: added routes for admin manage student

--- backend/routes.js
+++ backend/routes.js
@@ -1,5 +1,6 @@
 import {signUp, login, checkifloggedin} from "./auth-controller.js"
-import {createApproverAccount, searchApproverByName, filterNameAscending, filterNameDescending, deleteApprover, editApprover, approverLogin} from "./admin-approver-controller.js"
+import { searchApproverByName, filterNameAscending, filterNameDescending, deleteApprover, editApprover, approverLogin} from "./admin-approver-controller.js"
+import { sortStudentByStudentNum, assignAdviser, sortStudentByName, getPendingStudent} from "./admin-student-controller.js";
 
 const setUpRoutes = (app) => {
     app.get("/", (req, res) => { res.send("API Home") });
@@ -11,7 +12,11 @@ const setUpRoutes = (app) => {
     app.get("/sort-approver-by-name-desc", filterNameDescending);
     app.post("/delete-approver", deleteApprover);
     app.post("/edit-approver", editApprover); 
-    app.post("/approver-login", approverLogin);   
+    app.post("/approver-login", approverLogin);  
+    app.get("/get-pending-student", getPendingStudent);
+    app.get("/sort-student-by-name",sortStudentByName);
+    app.get("/sort-student-by-sn",  sortStudentByStudentNum);
+    app.post("/assign-adviser", assignAdviser);
   }
   
   export default setUpRoutes;
