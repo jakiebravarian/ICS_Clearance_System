97a7b5f3a69bfc484eac5e152d8eee4149497568
bautistaAD <aqbautista@up.edu.ph>
2023-06-05 04:40:53 +0800

    fix: updates

--- backend/admin-student-controller.js
+++ backend/admin-student-controller.js
@@ -66,53 +66,45 @@ const assignAdviser = async (req,res) => {
     }
     catch(err)
     {
-        res.status(500).send('An error occurred'); 
+        // res.status(500).send('An error occurred'); 
+        res.send(err);
     }
 }
 
+// mali
 const sortStudentByStudentNum = async (req,res) => {
     try{
-        const sortByStudNum =  await User.find({}).sort({studentNumber: "asc"});
+        const sortByStudNum =  await User.find({}).sort({studentNumber: "desc"});
         res.send(sortByStudNum);
+        // console.log("sn" + sortByStudNum);
     }catch(err){
-        res.status(500).send('An error occurred');
+        // res.status(500).send('An error occurred');
+        res.send(err);
     }
 }
 
 const sortStudentByName = async (req,res) => {
 
-    function returnResult(student){
-        if(!student)
-        {
-            res.send({found: false});
-        }
-        else
-        {
-            res.send(student);
-        }
-    };
 
     try{
-        //check first if search name is not empty
-        const searchName = req.body.search;
-        
-        if(searchName == ""){
-            const sortStudentName = await User.find({userType: "Student"}).sort({firstName: 'desc', lastName: 'desc'})
-            returnResult(sortStudentName)
-        }
-        else
-        {
-            const sortStudentName = await User.find({userType: "Student", $text: { $search: searchName }}).sort({firstName: 'desc', lastName: 'desc'})
-            if(!sortStudentName){
-                res.send({found: false});
-            }
-            else{
-                returnResult(sortStudentName);
-            }
-        } 
+        const sortbyName =  await User.find({}).sort({lastName: "asc"});
+        // res.send(sortbyName);
+        // console.log(sortbyName);
+        res.send(sortbyName)
       }catch(err){
-        res.status(500).send('An error occurred');
+       // res.status(500).send('An error occurred');
+       res.send(err);
       }
 }
 
-export {sortStudentByName, getPendingStudent, assignAdviser, sortStudentByStudentNum}
+const getStudent = async (req, res) => {
+    try{
+        const student =  await User.find({});
+        res.send(student)
+      }catch(err){
+       // res.status(500).send('An error occurred');
+       res.send(err);
+      }
+}
+
+export {sortStudentByName, getPendingStudent, assignAdviser, sortStudentByStudentNum, getStudent}
