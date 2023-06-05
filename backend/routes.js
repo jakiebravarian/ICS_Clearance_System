febcf7b7fcf3887a811cb1d71cc67be1e7c7cf11
bautistaAD <aqbautista@up.edu.ph>
2023-06-05 20:06:13 +0800

    chore:: update routes

--- backend/routes.js
+++ backend/routes.js
@@ -2,6 +2,7 @@ import { signUp, login, checkifloggedin } from "./auth-controller.js";
 import { getAllStudents, getCurrentStudent, submitApplication, viewStudentClearanceStatus, updateStep, updateStudentSubmission,closeApplication,getCurrentApplication, getCurrentApplications} from "./student-controller.js";
 import { searchApproverByName, filterNameAscending, filterNameDescending, deleteApprover, editApprover, approverLogin, getApprover} from "./admin-approver-controller.js"
 import { sortStudentByStudentNum, assignAdviser, sortStudentByName, getPendingStudent, getStudent} from "./admin-student-controller.js";
+import { getAllPendingApplications, searchStudentByName, searchStudentByStudentNumber, filterApplicationsByDate, filterApplicationsByAdviser, filterApplicationsByStep, sortApplicationsByDate, sortApplicationsByName, approveApplicationAtCurrentStep, returnAppAtCurrentStep, createApprover } from './approver-controller.js';
 
 const setUpRoutes = (app) => {
   app.get("/", (req, res) => {
@@ -26,16 +27,29 @@ const setUpRoutes = (app) => {
   app.put("/close-application", closeApplication);
   app.get("/get-student", getStudent);
 
-  //approver routes
+  //admin-approver routes
   app.post("/search-approver-by-name", searchApproverByName);
-  app.post("/sort-approver-by-name-asc", filterNameAscending);
-  app.post("/sort-approver-by-name-desc", filterNameDescending);
+  app.get("/sort-approver-by-name-asc", filterNameAscending);
+  app.get("/sort-approver-by-name-desc", filterNameDescending);
   app.get("/get-current-application",getCurrentApplication);
   app.get("/get-current-applications",getCurrentApplications);
   app.post("/delete-approver", deleteApprover);
   app.post("/edit-approver", editApprover); 
   app.post("/approver-login", approverLogin);  
   app.get("/get-approver", getApprover);
+
+  //approver routes
+  app.post("/create-approver", createApprover);
+  app.get("/get-all-pending-applications", getAllPendingApplications);
+  app.get("/search-student-by-name", searchStudentByName);
+  app.get("/search-student-by-student-number", searchStudentByStudentNumber);
+  app.get("/filter-applications-by-date", filterApplicationsByDate);
+  app.get("/filter-applications-by-adviser", filterApplicationsByAdviser);
+  app.get("/filter-applications-by-step", filterApplicationsByStep);
+  app.get("/sort-applications-by-date", sortApplicationsByDate);
+  app.get("/sort-applications-by-name", sortApplicationsByName);
+  app.put("/approve-application-at-current-step", approveApplicationAtCurrentStep);
+  app.put("/return-application-at-current-step", returnAppAtCurrentStep);
   
   //admin routes
   app.post("/assign-adviser", assignAdviser);
